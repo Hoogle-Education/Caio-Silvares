15f49a33510e5634faa9cf168d39b6ca0bec7090
hgrafa <hugorafell@gmail.com>
2023-03-31 23:40:18 -0300

    adding estoque

---
+++ tests/Estoque/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //        int contador = 1; // inicialização
+        //        while (contador <= 10) { // validação
+        //            contador++; // atualização
+        //            System.out.println(contador);
+        //        }
+        //        for(/*inicialização*/;/*validação*/;/*atualização*/){
+        //
+        //        }
+
+        for(int contador = 0; contador <= 10; contador++){
+            System.out.println(contador);
+        }
+    }
+}

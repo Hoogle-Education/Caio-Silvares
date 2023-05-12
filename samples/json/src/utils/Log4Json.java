12c433301f4ddf8f7471f97c1a0abdafc2a1b376
hgrafa <hugorafell@gmail.com>
2023-05-12 10:47:58 -0300

    updating annotations library service

--- samples/json/src/utils/Log4Json.java
+++ samples/json/src/utils/Log4Json.java
@@ -1,29 +1,61 @@
 package utils;
 
 import models.User;
+import models.annotations.JsonIgnore;
+import models.annotations.JsonKey;
 import models.annotations.JsonParser;
 import models.annotations.Jsonable;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Log4Json {
 
-    public static String serialize(User user) throws Exception {
-        String aux = "{";
+    public static String toJson(Object object) throws Exception {
+        if (!isJsonable(object))
+            return "{ not a jsonable }";
 
-        if (!isJsonable(user))
-            return aux + "}";
-
-        runObjectParsers(user);
-        aux += getObjectAsJson(user);
-        return aux + "}";
+        runObjectParsers(object);
+        var jsonString = getObjectAsJsonString(object);
+        return "{\n" + jsonString + "\n }";
     }
 
-    private static String getObjectAsJson(User user) {
-        String aux = "";
-        // TODO convert object fields to Json model
-        return "";
+    private static String getObjectAsJsonString(Object object)
+            throws IllegalAccessException {
+        var jsonElements = getKeyValueMapFromObject(object);
+
+        var quoteMark = "\"";
+        var tab = "\t";
+        var jsonString = jsonElements.entrySet()
+                .stream()
+                .map(entry -> tab + quoteMark + entry.getKey()
+                        + quoteMark + " : " + quoteMark
+                        + entry.getValue() + quoteMark)
+                .collect(Collectors.joining(",\n"));
+
+
+        return jsonString;
+    }
+
+    private static Map<String, String> getKeyValueMapFromObject(Object object) throws IllegalAccessException {
+        Map<String, String> jsonElements = new HashMap<>();
+        Class<?> objectClass = object.getClass();
+
+        for(var field : objectClass.getDeclaredFields()) {
+            field.setAccessible(true);
+            if(field.isAnnotationPresent(JsonIgnore.class))
+                continue;
+
+            var key = getKey(field);
+            var value = (String) field.get(object).toString();
+            jsonElements.put(key, value);
+        }
+
+        return jsonElements;
     }
 
     private static boolean isJsonable(Object object) {
@@ -34,7 +66,7 @@ public class Log4Json {
 
         Class<?> objectClass = object.getClass();
         if(!objectClass.isAnnotationPresent(Jsonable.class)){
-            System.err.println("This object is not maked as jsonable");
+            System.err.println("This object is not marked as jsonable");
             return false;
         }
 
@@ -50,5 +82,17 @@ public class Log4Json {
                 method.invoke(object);
             }
         }
+    }
+
+    public static String getKey(Field field) {
+        var key = field.getName();
+
+        if(field.isAnnotationPresent(JsonKey.class)) {
+            var annotationKey = field.getAnnotation(JsonKey.class).value();
+
+            key = (annotationKey.isBlank()) ? key : annotationKey;
+        }
+
+        return key;
     }
 }
